6e6ef9d4606c11de3565f15902f58dd9d0ca2748
Ana Flavia Rodrigues Lima <ana.lima@fpf.br>
2018-03-02 16:06:18 -0400

    Teste em uma api REST

---
+++ leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuarioWSTest.java
@@ -0,0 +1,14 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Test;
+/**
+ * Created by ana on 01/03/18.
+ */
+public class UsuarioWSTest {
+    @Test
+    public void deveRetornarListaDeUsuarios (){
+        //XmlPath path = get("/usuarios?_format=xml").andReturn().xmlPath();
+
+    }
+
+}
